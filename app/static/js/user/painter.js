9dcd3ec553020b4b65b6a61c307ac9b1be8dad4e
DSRoden <Daniel.S.Roden@gmail.com>
2014-08-15 15:42:00 -0500

    Painter

--- app/static/js/user/painter.js
+++ app/static/js/user/painter.js
@@ -2,7 +2,38 @@
   'use strict';
 
   $(document).ready(function(){
-    alert('painter');
+    $('#controls > button').click(addBox);
+    $('#colors').on('click', '.color', pallete);
+    $('#canvas').on('mouseenter', '.pixel', paintPixel);
+    makeCanvas();
   });
+
+  function paintPixel(){
+    var color = $('#selected').css('background-color');
+    $(this).css('background-color', color);
+  }
+
+  function makeCanvas(){
+    alert('make canvas');
+    for(var i = 0; i < 5000; i++){
+      var $pixel = $('<div>');
+      $pixel.addClass('pixel');
+      $('#canvas').append($pixel);
+    }
+  }
+
+  function addBox(){
+    var color = $('#controls > input').val(),
+      $color = $('<div>');
+
+    $color.addClass('color');
+    $color.css('background-color', color);
+    $('#colors').append($color);
+  }
+
+  function pallete(){
+    var chosenColor = $(this).css('background-color');
+    $('#selected').css('background-color', chosenColor);
+  }
 
 })();
